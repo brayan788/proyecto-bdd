bc83e356e45280c5c0c50d250cbc15ddfd51b895
brayan788 <brayan.gonzalez@uc.cl>
2022-05-18 17:58:21 -0400

    pagina

--- Sites/consultas/naves.php
+++ Sites/consultas/naves.php
@@ -7,7 +7,8 @@
     require("../config/conexion.php");
     $inicio = $_POST["fecha1"];
     $final = $_POST["fecha2"];
-    $query = "SELECT aeronave, COUNT(aeronave) FROM vuelo, tiene_vuelo WHERE vuelo.id =tiene_vuelo.idvuelo AND vuelo.realizado='realizado' GROUP BY aeronave;";
+    $query = "SELECT aeronave, COUNT(aeronave) FROM vuelo, tiene_vuelo WHERE vuelo.id =tiene_vuelo.idvuelo AND
+    vuelo.realizado='realizado' AND vuelo.fecha_salida>='$inicio' GROUP BY aeronave;";
     $result = $db -> prepare($query);
     $result -> execute();
     $vuelos = $result -> fetchAll();
@@ -15,17 +16,8 @@
 
 <table>
 <tr>
-    <th>ID</th>
-    <th>Estado</th>
     <th>Codigo</th>
-	<th>Fecha_salida</th>
-	<th>Fecha_llegada</th>
-	<th>Fecha_propuesta</th>
-	<th>Velocidad</th>
-	<th>Altitud</th>
-	<th>Tipo_vuelo</th>
-	<th>Max_pasajero</th>
-	<th>Realizado</th>
+    <th>Cantidad de viajes</th>
 </tr>
   <?php
 	foreach ($vuelos as $vuelo) {
