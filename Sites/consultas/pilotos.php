3badb7fb884dd8259d7e9662dc374034ec8573b1
brayan788 <brayan.gonzalez@uc.cl>
2022-05-18 15:10:42 -0400

    pagina

--- Sites/consultas/pilotos.php
+++ Sites/consultas/pilotos.php
@@ -7,7 +7,6 @@
 
   #Se obtiene el valor del input del usuario
   $fecha = $_POST["fecha"];
-  pg_exec("ALTER USER 'grupo82' SET datestyle TO 'European'")
  	$query = "SELECT id, catergoria, pasaporte FROM documento_p WHERE inicio<='$fecha' and termino>'$fecha';";
 
   #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
