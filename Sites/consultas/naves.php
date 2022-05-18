26353a3e44cfe1d912f23003e8a1ac53ae21b824
brayan788 <brayan.gonzalez@uc.cl>
2022-05-18 17:38:33 -0400

    pagina

--- Sites/consultas/naves.php
+++ Sites/consultas/naves.php
@@ -29,7 +29,7 @@
 </tr>
   <?php
 	foreach ($vuelos as $vuelo) {
-  		echo "<tr> <td>$vuelo[0]</td> <td>$vuelo[1]</td> <td>$vuelo[2]</td> <td>$vuelo[3]</td> <td>$vuelo[4]</td> <td>$vuelo[5]</td> <td>$vuelo[6]</td> <td>$vuelo[7]</td> <td>$vuelo[8]</td> <td>$vuelo[9]</td> <td>$vuelo[10]</td> </tr>";
+  		echo "<tr> <td>$vuelo[0]</td> <td>$vuelo[1]</td> </tr>";
 	}
   ?>
 	</table>
