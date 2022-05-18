a416722e70b356cbbfcf9081c54be7149e9375cb
brayan788 <brayan.gonzalez@uc.cl>
2022-05-18 09:54:33 -0400

    pagina

--- Sites/consultas/dosicao.php
+++ Sites/consultas/dosicao.php
@@ -13,21 +13,25 @@
   $result -> execute();
   $dataCollected = $result -> fetchAll(); #Obtiene todos los resultados de la consulta en forma de un arreglo
   ?>
-
-  <table>
+<table>
     <tr>
       <th>ID</th>
-      <th>Nombre</th>
-      <th>Altura</th>
-      <th>Peso</th>
-      <th>Exp Base</th>
-      <th>Tipo</th>
+      <th>Estado</th>
+      <th>Codigo</th>
+	  <th>Fecha_salida</th>
+	  <th>Fecha_llegada</th>
+	  <th>Fecha_propuesta</th>
+	  <th>Velocidad</th>
+	  <th>Altitud</th>
+	  <th>Tipo_vuelo</th>
+	  <th>Max_pasajero</th>
+	  <th>Realizado</th>
     </tr>
   <?php
-  foreach ($dataCollected as $p) {
-    echo "<tr> <td>$p[0]</td> <td>$p[1]</td> <td>$p[2]</td> <td>$p[3]</td> <td>$p[4]</td> <td>$p[5]</td> </tr>";
-  }
+	foreach ($vuelos as $vuelo) {
+  		echo "<tr> <td>$vuelo[0]</td> <td>$vuelo[1]</td> <td>$vuelo[2]</td> <td>$vuelo[3]</td> <td>$vuelo[4]</td> <td>$vuelo[5]</td> <td>$vuelo[6]</td> <td>$vuelo[7]</td> <td>$vuelo[8]</td> <td>$vuelo[9]</td> <td>$vuelo[10]</td> </tr>";
+	}
   ?>
-  </table>
+	</table>
 
 <?php include('../templates/footer.html'); ?>
