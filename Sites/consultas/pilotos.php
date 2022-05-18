5a33d6be86acc84f045096505627ad1515c356cb
brayan788 <brayan.gonzalez@uc.cl>
2022-05-18 12:08:30 -0400

    pagina

--- Sites/consultas/pilotos.php
+++ Sites/consultas/pilotos.php
@@ -8,7 +8,11 @@
   #Se obtiene el valor del input del usuario
   $fecha = $_POST["fecha"];
   #Se construye la consulta como un string
- 	$query = "SET DATESTYLE TO 'European'; SELECT id, catergoria, pasaporte FROM documento_p WHERE inicio<'$fecha';";
+  $query = "SET DATESTYLE TO 'European'"
+  $result = $db -> prepare($query);
+	$result -> execute();
+  $pilotos = $result -> fetchAll();
+ 	$query = "SELECT id, catergoria, pasaporte FROM documento_p WHERE inicio<'$fecha';";
 
   #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
 	$result = $db -> prepare($query);
