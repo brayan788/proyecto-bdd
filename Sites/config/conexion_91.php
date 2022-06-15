eede932d2ddb65ebfc80e9cb2c73534f1c7eecb5
Dyotson <maxmilitzer@gmail.com>
2022-06-15 19:17:30 -0400

    AAAAAA

--- Sites/config/conexion_91.php
+++ Sites/config/conexion_91.php
@@ -3,7 +3,12 @@
     #Pide las variables para conectarse a la base de datos.
     require('data_91.php'); 
     # Se crea la instancia de PDO
-    $db = new PDO("pgsql:dbname=$databaseName;host=localhost;port=5432;user=$user;password=$password");
+    # $db = new PDO("pgsql:dbname=$databaseName;host=localhost;port=5432;user=$user;password=$password");
+    $sname= "localhost";
+    $unmae= "$user";
+    $password = "$password";
+    $db_name = "$databaseName";
+    $conn = mysqli_connect($sname, $unmae, $password, $db_name);
   } catch (Exception $e) {
     echo "No se pudo conectar a la base de datos: $e";
   }
