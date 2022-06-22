a43455d23f3970a1caf577c0981f868426fa1447
brayan788 <brayan.gonzalez@uc.cl>
2022-06-22 18:08:51 -0400

    arreglo

--- Sites/consultas/admin.php
+++ Sites/consultas/admin.php
@@ -25,7 +25,6 @@
       <th>Codigo</th>
 	  <th>Fecha_salida</th>
 	  <th>Fecha_llegada</th>
-	  <th>Fecha_propuesta</th>
 	  <th>Velocidad</th>
 	  <th>Altitud</th>
 	  <th>Tipo_vuelo</th>
@@ -34,7 +33,7 @@
     </tr>
   <?php
 	foreach ($vuelos as $vuelo) {
-  		echo "<tr> <td>$vuelo[0]</td> <td>$vuelo[1]</td> <td>$vuelo[2]</td> <td>$vuelo[3]</td> <td>$vuelo[4]</td> <td>$vuelo[5]</td> <td>$vuelo[6]</td> <td>$vuelo[7]</td> <td>$vuelo[8]</td> <td>$vuelo[9]</td> <td>$vuelo[10]</td> </tr>";
+  		echo "<tr> <td>$vuelo[0]</td> <td>$vuelo[1]</td> <td>$vuelo[2]</td> <td>$vuelo[3]</td> <td>$vuelo[4]</td> <td>$vuelo[5]</td> <td>$vuelo[6]</td> <td>$vuelo[7]</td> <td>$vuelo[8]</td> <td>$vuelo[9]</td> </tr>";
 	}
   ?>
 	</table>
