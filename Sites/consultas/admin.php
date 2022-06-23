f6658354187e14b6a38c7f7d338d9a2cb9f8f438
brayan788 <brayan.gonzalez@uc.cl>
2022-06-22 22:20:20 -0400

    arreglo

--- Sites/consultas/admin.php
+++ Sites/consultas/admin.php
@@ -35,7 +35,7 @@
     </tr>
   <?php
 	foreach ($vuelos as $vuelo) {
-  		echo "<tr> <td>$vuelo[0]</td> <td>$vuelo[1]</td> <td>$vuelo[2]</td> <td>$vuelo[3]</td> <td>$vuelo[4]</td> <td>$vuelo[5]</td> <td>$vuelo[6]</td> <td>$vuelo[7]</td> <td>$vuelo[8]</td> <td>$vuelo[9]</td> <td> <a href="destino.php?variable1= $vuelo[0]">Aprobar</a> </td> </tr>";
+  		echo "<tr> <td>$vuelo[0]</td> <td>$vuelo[1]</td> <td>$vuelo[2]</td> <td>$vuelo[3]</td> <td>$vuelo[4]</td> <td>$vuelo[5]</td> <td>$vuelo[6]</td> <td>$vuelo[7]</td> <td>$vuelo[8]</td> <td>$vuelo[9]</td> <a href="destino.php?variable1= $vuelo[0]">Aprobar</a> </tr>";
 	}
   ?>
 	</table>
