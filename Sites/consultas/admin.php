9e71c68b9434bf3530d80728b15a89d7ba0d22aa
brayan788 <brayan.gonzalez@uc.cl>
2022-06-22 21:55:25 -0400

    arreglo

--- Sites/consultas/admin.php
+++ Sites/consultas/admin.php
@@ -35,7 +35,7 @@
     </tr>
   <?php
 	foreach ($vuelos as $vuelo) {
-  		echo "<tr> <td>$vuelo[0]</td> <td>$vuelo[1]</td> <td>$vuelo[2]</td> <td>$vuelo[3]</td> <td>$vuelo[4]</td> <td>$vuelo[5]</td> <td>$vuelo[6]</td> <td>$vuelo[7]</td> <td>$vuelo[8]</td> <td>$vuelo[9]</td> <td><input type="hidden" name="numero_cliente" value= $vuelo[0]>
+  		echo "<tr> <td>$vuelo[0]</td> <td>$vuelo[1]</td> <td>$vuelo[2]</td> <td>$vuelo[3]</td> <td>$vuelo[4]</td> <td>$vuelo[5]</td> <td>$vuelo[6]</td> <td>$vuelo[7]</td> <td>$vuelo[8]</td> <td>$vuelo[9]</td> <td><input type="hidden" name="numero_cliente" value= "$vuelo[0]">
 		<input type="submit" value="Aprobar"></td>   </tr>";
 	}
   ?>
