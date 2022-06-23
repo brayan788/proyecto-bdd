48f3cc190fca410c51e1726125631e14bd0e0856
brayan788 <brayan.gonzalez@uc.cl>
2022-06-22 23:33:47 -0400

    arreglo

---
+++ Sites/consultas/rechazar.php
@@ -0,0 +1,19 @@
+<?php include('../templates/header.html');   ?>
+
+<body>
+
+<?php 
+	session_start();
+	if ($_SESSION['type'] !== "admin") {
+		die('Acceso Denegado');
+	}?>
+
+<?php
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("../config/conexion_82.php");
+  $id = $_POST["id"];
+ 	$query = "UPDATE fpl SET estado = 'rechazado' WHERE id='$id';";
+	$result = $db -> prepare($query);
+	$result -> execute();
+    header('Location:' . getenv('HTTP_REFERER'));
+  ?>
