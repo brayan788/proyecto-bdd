4e193d2afdedc99479f29cdb804514d63659bc02
brayan788 <brayan.gonzalez@uc.cl>
2022-06-23 11:16:39 -0400

    arreglo

--- Sites/consultas/compania.php
+++ Sites/consultas/compania.php
@@ -9,29 +9,37 @@
 <body>
 <?php
   #Llama a conexión, crea el objeto PDO y obtiene la variable $db
-  require("../config/conexion.php");
+  require("../config/conexion_82.php");
 
   #Se obtiene el valor del input del usuario
-  $fecha = $_POST["fecha"];
- 	$query = "SELECT id, catergoria, pasaporte FROM documento_p WHERE inicio<='$fecha' and termino>'$fecha';";
+  $com = $_ SESSION['username'];
+ 	$query = "SELECT * FROM vuelo WHERE codigo LIKE '$com%' AND estado='aprobado';";
 
   #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
 	$result = $db -> prepare($query);
 	$result -> execute();
-	$pilotos = $result -> fetchAll();
+	$vuelos = $result -> fetchAll();
   ?>
 
   <table>
     <tr>
       <th>ID</th>
-      <th>Categoria</th>
-      <th>Pasaporte</th>
+      <th>Estado</th>
+      <th>Codigo</th>
+	  <th>Fecha_salida</th>
+	  <th>Fecha_llegada</th>
+	  <th>Velocidad</th>
+	  <th>Altitud</th>
+	  <th>Tipo_vuelo</th>
+	  <th>Max_pasajero</th>
+	  <th>Realizado</th>
     </tr>
-  
-      <?php
-        foreach ($pilotos as $p) {
-          echo "<tr><td>$p[0]</td><td>$p[1]</td><td>$p[2]</td></tr>";
-      }
+  <?php
+	foreach ($vuelos as $vuelo) {
+  		echo "<tr> <td>$vuelo[0]</td> <td>$vuelo[1]</td> <td>$vuelo[2]</td> <td>$vuelo[3]</td> <td>$vuelo[4]</td> <td>$vuelo[5]</td> <td>$vuelo[6]</td> <td>$vuelo[7]</td> <td>$vuelo[8]</td> <td>$vuelo[9]</td> </tr>";
+	}
+  ?>
+	</table>
       ?>
       
   </table>
