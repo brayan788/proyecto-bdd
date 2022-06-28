5e87dd6a604c8d18e3d8b53d5863dea307a06c1a
scagender <jpverafortune@uc.cl>
2022-06-28 19:44:05 -0400

    B

--- Sites/consultas/pasajeros2.php
+++ Sites/consultas/pasajeros2.php
@@ -22,9 +22,9 @@
             re.fecha_nacimiento_pasajero
             FROM vuelos vu 
             INNER JOIN reservas re ON vu.vuelo_id=re.vuelo_id 
-            INNER JOIN aerodromos ae ON ae.aerodromo_id=vu.aerodromo_salida_id
+            INNER JOIN aerodromos ae ON (ae.aerodromo_id=vu.aerodromo_salida_id 
             INNER JOIN aerodromos ae2 ON ae2.aerodromo_id=vu.aerodromo_llegada_id
-            WHERE re.nombre_pasajero LIKE '$pas%' AND vu.fecha_salida>='$fecha' AND ae.nombre_ciudad='$origen' AND ae.nombre_ciudad='$destino';";
+            WHERE re.nombre_pasajero LIKE '$pas%' AND vu.fecha_salida>='$fecha' AND ae.nombre_ciudad='$origen' AND ae2.nombre_ciudad='$destino';";
   $result = $db -> prepare($query);
   $result -> execute();
   $reservas = $result -> fetchAll();
