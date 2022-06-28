5eaa72d9f61319a79c3cdaa17a1ff52602b1af2f
scagender <jpverafortune@uc.cl>
2022-06-28 19:03:59 -0400

    Q

--- Sites/consultas/pasajeros2.php
+++ Sites/consultas/pasajeros2.php
@@ -56,3 +56,9 @@
 	</table>
 
 <?php include('../templates/footer.html'); ?>
+
+<form action="../consultas/pasajeros.php" method="get">
+    <input type="submit" value="Volver">
+</form>
+</body>
+</html>
