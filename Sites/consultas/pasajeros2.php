223e06d5d18ac2a54c2291ed8e15d033e083958a
scagender <jpverafortune@uc.cl>
2022-06-28 20:24:48 -0400

    Ni idea cuantos llevo

--- Sites/consultas/pasajeros2.php
+++ Sites/consultas/pasajeros2.php
@@ -20,8 +20,7 @@
             FROM vuelos vu
             JOIN reservas re ON vu.vuelo_id=re.vuelo_id 
             JOIN aerodromos ae ON ae.aerodromo_id=vu.aerodromo_salida_id
-            JOIN aerodromos ae2 ON ae2.aerodromo_id=vu.aerodromo_llegada_id
-            WHERE ae.nombre_ciudad='$origen' AND ae2.nombre_ciudad='$destino';";
+            WHERE vu.estado LIKE 'pendiente' AND fecha_salida>='$fecha' AND ae.nombre_ciudad='$origen';";
   $result = $db -> prepare($query);
   $result -> execute();
   $reservas = $result -> fetchAll();
