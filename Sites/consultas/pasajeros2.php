8803a6bb4c157168c808a1170db7580c61e0d642
scagender <jpverafortune@uc.cl>
2022-06-28 20:20:27 -0400

    AAAAAAA

--- Sites/consultas/pasajeros2.php
+++ Sites/consultas/pasajeros2.php
@@ -17,8 +17,8 @@
     $destino = $_POST["destino"];
     $fecha = $_POST["fecha3"];
     $query = "SELECT re.reserva_id, re.codigo_reserva, re.numero_ticket, re.vuelo_id, re.pasaporte_comprador, re.nombre_comprador, re.nacionalidad_comprador, re.fecha_nacimiento_comprador, re.numero_asiento, re.clase, re.comida_y_maleta, re.pasaporte_pasajero, re.nombre_pasajero, re.nacionalidad_pasajero, re.fecha_nacimiento_pasajero
-            FROM reservas re
-            JOIN vuelos vu ON vu.vuelo_id=re.vuelo_id 
+            FROM vuelos vu
+            JOIN reservas re ON vu.vuelo_id=re.vuelo_id 
             JOIN aerodromos ae ON ae.aerodromo_id=vu.aerodromo_salida_id
             JOIN aerodromos ae2 ON ae2.aerodromo_id=vu.aerodromo_llegada_id
             WHERE ae.nombre_ciudad='$origen' AND ae2.nombre_ciudad='$destino';";
